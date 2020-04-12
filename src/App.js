8c51dc0b648003a915c7c086f04db51338543e5e
Gustavo Serao <gus1tavo@hotmail.com>
2020-04-11 22:37:33 -0300

    resolucao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
+import "./services/api"
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get("repositories").then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: 'Meu Respositorio Favorito',
+      url:'https://github.com/Gugusta/GoStack-Desafio-Conceitos-Node',
+      techs: ['Node.JS', 'ReactJS']
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    setRepositories(repositories.filter(
+      respository => respository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}> 
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
